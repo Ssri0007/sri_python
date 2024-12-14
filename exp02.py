23c78255ba4015e14eeceaaec93020a5ba0f106c
S.Sripãthi rão <sanakasai007@gmail.com>
2024-12-14 15:57:25 +0530

    Create exp02.py

---
+++ exp02.py
@@ -0,0 +1,16 @@
+s=["sri","sai","johnwick","jamesbond","jonsnow","jaime","arya stark","rob stark","sansa stark","antony stark"]
+s.insert(1,"MAD MAX")
+print(s)
+s.append("MARTHA")
+print(s)
+s.pop()
+print(s)
+n="SRI"
+print(len(n))
+s=["sri","sai","johnwick","jamesbond","jonsnow","jaime","arya stark","rob stark","sansa stark","antony stark"]
+s.pop()
+s.pop(1)
+s.pop(2)
+s.pop(3)
+print(s)
+
